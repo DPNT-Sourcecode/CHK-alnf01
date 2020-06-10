414d859cc872c43dd5a408374c3e17a5a59d6c21
sbx_user1051 <sbx_user1051@169.254.32.133>
2020-06-10 09:41:46 +0000

    Wed Jun 10 09:41:46 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,21 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+
+
+
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    public static void main(String ...argv) {
+        System.out.println("CHK_START");
+        checkout(null);
+
+        System.out.println("CHK_END");
+    }
+
+    public static Integer checkout(String skus) {
+        if(skus==null || skus.isEmpty()) return -1;
+
+        return 0;
     }
 }
+
