8b3aac0428432967b193df2c111befe41cb8c593
sbx_user1051 <sbx_user1051@169.254.32.133>
2020-06-10 09:47:46 +0000

    Wed Jun 10 09:47:46 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,7 +2,8 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CheckoutSolution {
@@ -17,8 +18,22 @@ public class CheckoutSolution {
     public static Integer checkout(String skus) {
         if(skus==null || skus.isEmpty()) return -1;
 
+
         return 0;
     }
+
+    public static boolean chekcValidSku(String skus) {
+        List<String> allowedChars = new ArrayList<>();
+        allowedChars.add("A");
+        allowedChars.add("B");
+        allowedChars.add("C");
+        allowedChars.add("D");
+        allowedChars.add("3A");
+        allowedChars.add("2B");
+
+        
+    }
 }
+
 
 
