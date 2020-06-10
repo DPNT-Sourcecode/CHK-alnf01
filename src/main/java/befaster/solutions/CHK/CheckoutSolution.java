ee7c5087630ffe314d6aa1f9cd75c4e0ed9222d7
sbx_user1051 <sbx_user1051@169.254.32.133>
2020-06-10 10:55:39 +0000

    Wed Jun 10 10:55:39 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -11,12 +11,12 @@ import java.util.Map;
 public class CheckoutSolution {
     public static void main(String ...argv) {
         System.out.println("CHK_START");
-        System.out.println("(AAABB)(210)[" + checkout("AAABB") + "]");
-        System.out.println("(BABDDCAC)(230)[" + checkout("BABDDCAC") + "]");
-        System.out.println("(BBBB)(120)[" + checkout("BBBB") + "]");
-        System.out.println("(B)(30)[" + checkout("B") + "]");
-        System.out.println("(AAAA)(200)[" + checkout("AAAA") + "]");
-        System.out.println("(ABCD)(115)[" + checkout("AC3A2B0") + "]");
+        System.out.println("(AAAAAAAAA)(380)[" + checkout("AAAAAAAAA") + "]");
+//        System.out.println("(BABDDCAC)(230)[" + checkout("BABDDCAC") + "]");
+//        System.out.println("(BBBB)(120)[" + checkout("BBBB") + "]");
+//        System.out.println("(B)(30)[" + checkout("B") + "]");
+//        System.out.println("(AAAA)(200)[" + checkout("AAAA") + "]");
+//        System.out.println("(ABCD)(115)[" + checkout("AC3A2B0") + "]");
 
         System.out.println("CHK_END");
     }
@@ -42,45 +42,58 @@ public class CheckoutSolution {
 
         //System.out.println("==>remainingProducts [" + remainingProducts + "]");
         int countProdA = countSpecialProducts(remainingProducts,"A");
+        int sumA =0 ;
         if(countProdA > 0 ) {
             int countProd5A = countProdA / 5;
             if (countProd5A > 0) countProdA = countProdA - countProd5A;
 
             int countProd3A = countProdA / 3;
             int countRestA = countProdA % 3;
-            int sumA = countProd3A * values.get("3A") + countRestA * values.get("A");
+            sumA = countProd3A * values.get("3A") + countRestA * values.get("A");
             System.out.println("======>A " + countProdA + "--" + countProd3A + "--" + countRestA + "--------" + sumA);
             remainingProducts = remainingProducts.replace("A", "");
         }
 
         //System.out.println("==>remainingProducts [" + remainingProducts + "]");
+        int sumB = 0;
         int countProdB = countSpecialProducts(remainingProducts,"B");
-        int countProd2B = countProdB/2;
-        int countRestB = countProdB%2;
-        int sumB = countProd2B*values.get("2B") + countRestB*values.get("B");
-        System.out.println("======>B " + countProdB + "--" + countProd2B + "--" + countRestB + "--------" + sumB);
-        remainingProducts = remainingProducts.replace("B","");
+        if(countProdB > 0) {
+            int countProd2B = countProdB / 2;
+            int countRestB = countProdB % 2;
+            sumB = countProd2B * values.get("2B") + countRestB * values.get("B");
+            System.out.println("======>B " + countProdB + "--" + countProd2B + "--" + countRestB + "--------" + sumB);
+            remainingProducts = remainingProducts.replace("B", "");
+        }
 
         //System.out.println("==>remainingProducts [" + remainingProducts + "]");
         int countProdC = countSpecialProducts(remainingProducts,"C");
-        int sumC = values.get("C") * countProdC;
-        System.out.println("======>C " +  sumC);
-        remainingProducts = remainingProducts.replace("C","");
+        int sumC = 0;
+        if(countProdC > 0) {
+            sumC = values.get("C") * countProdC;
+            System.out.println("======>C " + sumC);
+            remainingProducts = remainingProducts.replace("C", "");
+        }
 
         //System.out.println("==>remainingProducts [" + remainingProducts + "]");
         int countProdD = countSpecialProducts(remainingProducts,"D");
-        int sumD = values.get("D") * countProdD;
-        System.out.println("======>D " +  sumD);
-        remainingProducts = remainingProducts.replace("D","");
+        int sumD = 0;
+        if(countProdD > 0) {
+            sumD = values.get("D") * countProdD;
+            System.out.println("======>D " + sumD);
+            remainingProducts = remainingProducts.replace("D", "");
+        }
 
         //System.out.println("==>remainingProducts [" + remainingProducts + "]");
         int countProdE = countSpecialProducts(remainingProducts,"E");
-        int sumE = values.get("E") * countProdE;
-        System.out.println("======>E " +  sumE);
-        int countBFree = countProdE/2;
-        if(sumB> 0 ) sumB = sumB - countBFree*values.get("B");
-        if(sumB < 0) sumB=0;
-        remainingProducts = remainingProducts.replace("E","");
+        int sumE = 0;
+        if(countProdE > 0) {
+            sumE = values.get("E") * countProdE;
+            System.out.println("======>E " + sumE);
+            int countBFree = countProdE / 2;
+            if (sumB > 0) sumB = sumB - countBFree * values.get("B");
+            if (sumB < 0) sumB = 0;
+            remainingProducts = remainingProducts.replace("E", "");
+        }
 
         int sum = sumA + sumB + sumC + sumD;
 
@@ -136,5 +149,6 @@ public class CheckoutSolution {
 
 
 }
+
 
 
